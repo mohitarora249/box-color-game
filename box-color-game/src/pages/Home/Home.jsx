f508b5f5f46a1e0b60103865694a416977373b4b
mohit-arora <mohitarora249@gmail.com>
2021-05-07 20:02:02 +0530

    removed unused code

--- box-color-game/src/pages/Home/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import Board from "../../components/Board/Board";
-
-const Home = ({ rows, cols }) => {
-  return <Board rows={rows} cols={cols} />;
-};
-
-export default Home;
